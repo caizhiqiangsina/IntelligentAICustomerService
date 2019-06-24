59624923213a1a6d9018c3e32f2ebd5bb1c278e2
miracle@sina <miracle@sina.com>
2019-06-25 01:33:48 +0800

    dd

--- appsweb/push/controller/Index.php
+++ appsweb/push/controller/Index.php
@@ -7,6 +7,15 @@ class Index extends Controller
 {
     public function index()
     {
+        $this->login();
+//        return $this->fetch();
+    }
+    public function login()
+    {
+        return $this->fetch();
+    }
+    public function pc()
+    {
         return $this->fetch();
     }
 }
